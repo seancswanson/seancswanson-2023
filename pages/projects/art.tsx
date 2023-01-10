bfdfbdbc03c3717f261df9cc8230c2c21e7f2b4a
Sean Swanson <seancswan@gmail.com>
2023-01-10 00:13:14 -0800

    Now exporting default

--- pages/projects/art.tsx
+++ pages/projects/art.tsx
@@ -1,3 +1,5 @@
-export const Art = () => {
+const Art = () => {
   return <h1>Art Page</h1>;
 };
+
+export default Art;
--- pages/projects/dev.tsx
+++ pages/projects/dev.tsx
@@ -1,3 +1,5 @@
-export const Dev = () => {
+const Dev = () => {
   return <h1>Dev Page</h1>;
 };
+
+export default Dev;
