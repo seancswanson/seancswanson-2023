d20e23ff576f6625c0f1dea366b1338467d669b7
Sean Swanson <seancswan@gmail.com>
2023-07-15 12:44:10 -0700

    installed some missing deps

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,6 +1,8 @@
 import { remark } from "remark";
 import html from "remark-html";
 import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
 
 export async function getPostData(id: string) {
   const postsDirectory = "../data/posts/";
