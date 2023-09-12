89c3c186359df21fc1b1decf014db6b04a689b46
Sean Swanson <seancswan@gmail.com>
2023-09-11 18:45:12 -0700

    Took the breadcrumbs in on width.

--- components/Breadcrumbs.tsx
+++ components/Breadcrumbs.tsx
@@ -14,14 +14,14 @@ export const Breadcrumbs = ({ items }: BreadcrumbsProps) => {
     <div className="breadcrumbs mb-4 text-xs font-bold uppercase text-blue-500 opacity-50 transition-opacity duration-75 hover:opacity-100">
       /
       {items.map((item, index) => (
-        <span key={index} className="ml-2">
+        <span key={index} className="ml-1">
           <Link
             href={item.href}
             className=" border-b-2 border-transparent transition-all hover:border-blue-500"
           >
             {item.title}
           </Link>
-          {index < items.length - 1 && <span className="mx-2">/</span>}
+          {index < items.length - 1 && <span className="mx-1">/</span>}
         </span>
       ))}
     </div>
