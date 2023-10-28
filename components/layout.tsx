5b7e6d5392f11b9b59c3aeae864642bab0a43601
Sean Swanson <seancswan@gmail.com>
2023-10-28 15:30:39 -0700

    Updated CV link.

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,9 +1,19 @@
+import Head from "next/head";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Head>
+        <title>Sean Swanson | Web Developer | Seattle, WA </title>
+        <meta
+          name="description"
+          content="Portfolio for Sean Swanson | Web Developer | Seattle, WA"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header />
       <main className="mb-4 w-full max-w-[1010px] px-6">{children}</main>
       <Footer />
